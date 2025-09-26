e265d9e3a9b34d09d430ab345c887a5cb9b68839
IsamaDes <isamadesmond@gmail.com>
2025-09-26 04:30:12 +0100

    user signup

--- app.js
+++ app.js
@@ -1,41 +1,40 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+const express = require("express");
+const cors = require("cors");
+const routes = require("./routes");
+const { notFound, errorHandler } = require("./middleware/errorMiddleware");
+const swaggerUi = require("swagger-ui-express");
+const swaggerSpec = require("./config/swagger.js");
+const authMiddleware = require("./middleware/auth");
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+const app = express();
 
-var app = express();
+app.use(
+  cors({
+    origin: [
+      "https://todo-frontend-rosy-five.vercel.app",
+      "http://localhost:3000",
+      "http://localhost:5000",
+    ],
+    methods: "GET,POST,PUT,DELETE",
+    credentials: true,
+    allowedHeaders: "Content-Type,Authorization",
+  })
+);
 
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
-
-app.use(logger('dev'));
 app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerSpec));
 
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  next(createError(404));
+app.get("/", (req, res) => {
+  res.send("API is running");
 });
 
-// error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
-
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
+app.get("/api/profile", authMiddleware, async (req, res) => {
+  res.json({ profile: req.user });
 });
+app.use("/api", routes);
+
+app.use(errorHandler);
+app.use(notFound);
 
 module.exports = app;
