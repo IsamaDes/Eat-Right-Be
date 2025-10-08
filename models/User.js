546103df89c046f88f9c3fc51baf352fbe936314
IsamaDes <isamadesmond@gmail.com>
2025-10-08 01:42:50 +0100

    Registration completed

--- models/User.js
+++ models/User.js
@@ -1,6 +1,8 @@
 const mongoose = require("mongoose");
+const { v4: uuidv4 } = require("uuid");
 
 const UserSchema = new mongoose.Schema({
+  userId: { type: String, default: uuidv4 },
   name: { type: String, trim: true },
   email: {
     type: String,
