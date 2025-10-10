aafb2bc39cba9101efd0b7a8e7473e17413f3856
IsamaDes <isamadesmond@gmail.com>
2025-10-10 20:23:06 +0100

    updated cors

--- src/app.js
+++ src/app.js
@@ -1,4 +1,3 @@
-"use strict";
 const express = require("express");
 const app = express();
 const cors = require("cors");
@@ -6,33 +5,47 @@ const swaggerUi = require("swagger-ui-express");
 const swaggerSpec = require("./config/swagger");
 const apiRoutes = require("./routes");
 const { notFound, errorHandler } = require("./middleware/errorMiddleware");
-const allowedOrigins = process.env.NODE_ENV === "production"
-    ? ["https://eatright-theta.vercel.app"]
-    : ["http://localhost:5173"];
-app.use(cors({
+
+const allowedOrigins =
+  process.env.NODE_ENV === "production"
+    ? ["https://eatright-theta.vercel.app"] 
+    : ["http://localhost:5173"]; 
+
+
+app.use(
+  cors({
     origin: (origin, callback) => {
-        if (!origin)
-            return callback(null, true);
-        if (allowedOrigins.includes(origin)) {
-            callback(null, true);
-        }
-        else {
-            console.error(`❌ CORS blocked request from origin: ${origin}`);
-            callback(new Error("Not allowed by CORS"));
-        }
+      
+      if (!origin) return callback(null, true);
+
+      if (allowedOrigins.includes(origin)) {
+        callback(null, true);
+      } else {
+        console.error(`❌ CORS blocked request from origin: ${origin}`);
+        callback(new Error("Not allowed by CORS"));
+      }
     },
-    credentials: true,
+    credentials: true, 
     methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
     allowedHeaders: ["Content-Type", "Authorization"],
-}));
+  })
+);
+
+
 app.options("*", cors({
-    origin: allowedOrigins,
-    credentials: true,
+  origin: allowedOrigins,
+  credentials: true,
 }));
+
+
+
 app.use(express.json());
 app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerSpec));
+
 app.get("/", (req, res) => res.send("API is running"));
 app.use("/api", apiRoutes);
+
 app.use(errorHandler);
 app.use(notFound);
+
 module.exports = app;
