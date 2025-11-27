d3593cf1676687b0a2c5ef3bc5eeb8531220e33d
IsamaDes <isamadesmond@gmail.com>
2025-11-27 19:45:40 +0100

    Added caching to routes

--- src/routes/nutritionistRoutes.ts
+++ src/routes/nutritionistRoutes.ts
@@ -4,18 +4,21 @@ const router = express.Router();
 import protect from "../middleware/authMiddleware";
 import authorizeRoles from "../middleware/roleMiddleware";
 import {getNutritionistProfile, getClients, createMealPlan, getMealPlans, updateMealPlan, getMealPlanById} from "../controllers/nutritionist/nutritionistController";
+import { cacheMiddleware } from "../middleware/cacheMiddleware";
 
 router.use(protect);
 // Nutritionist-only routes
 router.get(
   "/profile",
   authorizeRoles("nutritionist"),
+  cacheMiddleware(req => `nutritionist:profile:${req.user?.id}`, 3600),
   getNutritionistProfile
 );
 
 router.get(
   "/clients",
   authorizeRoles("nutritionist"),
+  cacheMiddleware(req => `nutritionist:clients:${req.user?.id}`, 1800),
   getClients
 );
 
@@ -26,7 +29,9 @@ router.post(
   createMealPlan
 );
 
-router.get("/", authorizeRoles("admin", "nutritionist", "client"), getMealPlans);
+router.get("/", authorizeRoles("admin", "nutritionist", "client"),
+cacheMiddleware(req => `mealplans:user:${req.user?.id}`, 600),
+getMealPlans);
 
 router.put(
   "/:id",
@@ -37,6 +42,7 @@ router.put(
 router.get(
   "/:id",
   authorizeRoles("admin", "nutritionist", "client"),
+  cacheMiddleware(req => `mealplan:${req.params.id}`, 600),
   getMealPlanById
 );
 
